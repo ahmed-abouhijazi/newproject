8b2baefa748c6302c6b96c67306760c6d1b6d252
Ahmed ABOUHIJAZI <sabir.ahmed2402@gmail.com>
2020-03-25 22:04:37 +0100

    premier commit

---
+++ include/string.h
@@ -0,0 +1,16 @@
+#ifndef STRING_H
+#define STRING_H
+
+
+class string
+{
+    public:
+        string();
+        virtual ~string();
+
+    protected:
+
+    private:
+};
+
+#endif // STRING_H
---
+++ src/string.cpp
@@ -0,0 +1,11 @@
+#include "string.h"
+
+string::string()
+{
+    //ctor
+}
+
+string::~string()
+{
+    //dtor
+}
